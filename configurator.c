d2a3fbb366d34ca5f3b1cbda90772385245b676f
Vadoseg <145899163+Vadoseg@users.noreply.github.com>
2024-08-06 15:27:38 +0700

    Make header

--- configurator.c
+++ configurator.c
@@ -14,32 +14,7 @@
 #include <sys/types.h>
 #include <sys/mman.h>
 
-#define FATAL do { fprintf(stderr, "Error at line %d, file %s (%d) [%s]\n", \
-  __LINE__, __FILE__, errno, strerror(errno)); exit(1); } while(0)
-
-// Make header for defines
-#define POW_CONF_ADDR "0x83c03044"    // Address for pow configuration
-#define ON_OFF_ADDR   "0x83c03040 "   // Address for on/off and work.period
-
-#define POW_CONF_DATA "0x00000004"  // Data to conf pow step
-#define ON_OFF_DATA   "0x00100400"  // Data to on/off and work.period
-
-#define MAP_ADDR      0x83c02000    // First address for reading
-
-#define START_ADDR    0x83c03000    // First address for writing
-
-#define AMP_BIT_LEN 8               // Size of the First part in writing data
-#define PH_INCR_BIT_LEN 12          // Middle and Last size of parts in writing data
-#define CONC_BIT_LEN 32             // Full size of writing data
-
-// Constants from DDS
-#define F_CLK 200000000             // Frequency of zc-702
-#define BITS 16                     // Number of Bits in phase accumulator (Phase width) and phase increment value
-
-// Constants from devmem2
-#define MAP_SIZE 4096UL
-#define MAP_MASK (MAP_SIZE - 1)
-
+#include "configurator.h"
 
 int devmem(int argc, char **argv) {     // devmem2 to write in mem
     int fd;
